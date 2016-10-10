389f8e40a5eb1479ff1e6eeb2a943a5e34a169af
Manh T. Truong <manhattan.vn@gmail.com>
2016-10-10 21:35:07 +0700

    User can list the food item with order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,23 @@ class HomeController < ApplicationController
     def menu
         if params[:section]
             @section = Section.where(name: params[:section]).first
-        end
-    end
+			@food_item = if params[:order]
+							case params[:order]
+					when 'alphabet'
+						FoodItem.where(section: @section).order('name ASC')
+					when 'lowtohigh'
+						FoodItem.where(section: @section).order('price ASC')
+					when 'hightolow'
+						FoodItem.where(section: @section).order('price DESC')
+					else
+						nil
+					end
+				else
+					@section.food_items
+				end
+		end
+
+	end
 
     def contact
     end
