60191b4ec84837bdcd05d38ca0dd464221d3e18e
Truong Tuan Manh <tuanmanh.truong@vn.panasonic.com>
2016-10-10 17:59:44 +0000

    Generated scaffold for Food items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :food_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/menu', to: 'home#menu'
   get '/contact', to: 'home#contact'
