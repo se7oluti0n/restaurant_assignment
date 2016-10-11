cd2aeb0de3817003a25fd1dde7cc1a06d80244ba
Truong Tuan Manh <tuanmanh.truong@vn.panasonic.com>
2016-10-11 11:03:25 +0000

    Using render and template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,30 @@
 class HomeController < ApplicationController
-	def index
-		if params[:search]
-			@search_result = FoodItem.where(['lower(name) like ?', "%#{params[:search].downcase}%"])
-		end	
-	end
+    def index
+            if params[:search]
+                    @food_items = FoodItem.where(['lower(name) like ?', "%#{params[:search].downcase}%"])
+            end	
+    end
+
     def menu
         if params[:section]
             @section = Section.where(name: params[:section]).first
-			@food_item = if params[:order]
-							case params[:order]
-					when 'alphabet'
-						FoodItem.where(section: @section).order('name ASC')
-					when 'lowtohigh'
-						FoodItem.where(section: @section).order('price ASC')
-					when 'hightolow'
-						FoodItem.where(section: @section).order('price DESC')
-					else
-						nil
-					end
-				else
-					@section.food_items
-				end
-		end
+            @food_items = if params[:order]
+                                            case params[:order]
+                            when 'alphabet'
+                                    FoodItem.where(section: @section).order('name ASC')
+                            when 'lowtohigh'
+                                    FoodItem.where(section: @section).order('price ASC')
+                            when 'hightolow'
+                                    FoodItem.where(section: @section).order('price DESC')
+                            else
+                                    nil
+                            end
+                    else
+                            @section.food_items
+                    end
+        end
 
-	end
+    end
 
     def contact
     end
